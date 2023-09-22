cddf38ee4ae7517b0d57844edb6083eabe6def53
Andrey Luzan <andrejluzan@gmail.com>
2023-09-22 16:38:32 +0700

    Написана функция сравнения длины строки с максимальной допустимой длиной

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-function getRandomNumber (min, max) {
+function getRandomNumber(min, max) {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
   return result;
 }
 
-getRandomNumber();
+getRandomNumber(4, 20);
+
+function isNormalStringLenght(string, maxLength) {
+  if(string.length > maxLength) {
+    return false;
+  }
+
+  return true;
+}
+
+isNormalStringLenght('Кекс', 4);
+
