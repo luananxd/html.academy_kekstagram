916441276bc8cf34ccc46493020ea830865e3401
Andrey Luzan <andrejluzan@gmail.com>
2023-10-01 15:00:22 +0700

    Написаны функции по созданию комментария, пользователя и вывода случайного элемента массива

--- js/main.js
+++ js/main.js
@@ -6,12 +6,75 @@ function getRandomNumber(min, max) {
 getRandomNumber(4, 20);
 
 function isNormalStringLenght(string, maxLength) {
-  if(string.length > maxLength) {
-    return false;
-  }
-
-  return true;
+  return string.length > maxLength
 }
 
 isNormalStringLenght('Кекс', 4);
 
+// ----- Третье задание -----
+
+const DESCRIPTIONS = [
+  'На курорте в Майами',
+  'Как прекрасна осень!',
+  'С Виталиком на даче',
+  'Просто фото',
+  'Мой статус - на связи'
+];
+
+const NAMES = [
+  'Артём',
+  'Иннокентий',
+  'Иван',
+  'Джон',
+  'Дмитрий',
+  'Олег',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+let usersStartID = 1;
+let commentsID = [];
+
+function getRandomArrayElement (array) {
+  return array[getRandomNumber(0, array.length - 1)];
+}
+
+function getRandomUniqueNumber (min, max) {
+  while(true) {
+    let commentID = getRandomNumber(min, max);
+    if (!commentsID.includes(commentID)) {
+      commentsID.push(commentID);
+      return commentID;
+    } else {
+      continue;
+    }
+  }
+}
+
+function createComment () {
+  return {
+    id: getRandomUniqueNumber (1, 999),
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  }
+}
+
+function createUser () {
+  return {
+    id: ++usersStartID,
+    url: `photos/${usersStartID}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(1, 5)}, createComment),
+  }
+}
+
+let users = Array.from({length: 25}, createUser);
